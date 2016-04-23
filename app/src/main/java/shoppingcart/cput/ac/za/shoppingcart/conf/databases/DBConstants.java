03a8b5760aeea930c2e706f0b36a681158edd401
Braedy thebus <bthebus2@gmail.com>
2016-04-23 23:02:04 +0200

    added DBConstants.java and HashBitmapCache.java

---
+++ app/src/main/java/shoppingcart/cput/ac/za/shoppingcart/conf/databases/DBConstants.java
@@ -0,0 +1,12 @@
+package shoppingcart.cput.ac.za.shoppingcart.conf.databases;
+
+/**
+ * Author       : Braedy Thebus
+ * Stud num     : 213039168
+ * Email        : bthebus2@gmail.com
+ * Date created : 2016-04-23
+ */
+public class DBConstants {
+    public static final String DATABASE_NAME="shoppingCart";
+    public static final int DATABASE_VERSION=1;
+}
