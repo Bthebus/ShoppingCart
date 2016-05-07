b5af6bf9018de3961d2942acb28f22c28be7aa8e
Braedy thebus <bthebus2@gmail.com>
2016-05-07 23:57:03 +0200

    Updated

--- app/src/main/java/shoppingcart/cput/ac/za/shoppingcart/conf/databases/DBConstants.java
+++ app/src/main/java/shoppingcart/cput/ac/za/shoppingcart/conf/databases/DBConstants.java
@@ -7,6 +7,7 @@ package shoppingcart.cput.ac.za.shoppingcart.conf.databases;
  * Date created : 2016-04-23
  */
 public class DBConstants {
+
     public static final String DATABASE_NAME="shoppingCart";
     public static final int DATABASE_VERSION=5;
 }
