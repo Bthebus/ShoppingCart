8d13323bec6cfd1cc9921104de1ebd408349fe5d
Braedy thebus <bthebus2@gmail.com>
2016-05-07 23:56:40 +0200

    Initial Commit

--- app/src/androidTest/java/shoppingcart/cput/ac/za/shoppingcart/repository/SaleRepositoryTest.java
+++ app/src/androidTest/java/shoppingcart/cput/ac/za/shoppingcart/repository/SaleRepositoryTest.java
@@ -1,12 +1,10 @@
 package shoppingcart.cput.ac.za.shoppingcart.repository;
 
-import shoppingcart.cput.ac.za.shoppingcart.domain.Personal.User;
-
 /**
  * Author       : Braedy Thebus
  * Stud num     : 213039168
  * Email        : bthebus2@gmail.com
- * Date created : 2016-04-23
+ * Date created : 2016-05-07
  */
-public interface UserRepository extends Repository<User,Long>{
+public class SaleRepositoryTest {
 }
